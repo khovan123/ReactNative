d1bb16623d0efe49c679a997c9a974e92d7c155a
khovan <minhpnq1807@gmail.com>
2024-03-03 18:34:30 +0700

    using expo Bottom Tab Bar for Navivator

--- DrawerNavigation/App.js
+++ DrawerNavigation/App.js
@@ -1,5 +1,45 @@
-import { Text } from 'react-native';
-
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 export default function App() {
-  return <Text>Todo...</Text>;
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "red" },
+          headerTintColor: "#fff",
+          tabBarActiveTintColor: "red",
+          tabBarActiveBackgroundColor: "pink",
+        }}
+        initialRouteName="Welcome"
+      >
+        <BottomTab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            tabBarLabel: "Welcome Screen",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            tabBarLabel: "User Screen",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
 }
